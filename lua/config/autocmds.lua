d85af1478ca93c89b9b11335a60269746ce9e377
Aevox121 <1213667801@qq.com>
2026-05-06 18:52:59 +0800

    feat(autocmds): markdown/text 只开 wrap 不开 spell
    去掉 LazyVim 默认的 wrap_spell 组，避免中英混排下 spell 满屏红波浪线。

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,7 +5,16 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- 删掉 LazyVim 的 wrap_spell 组：原版会在 markdown/text/tex 里同时开 wrap + spell，
+-- 中文/中英混排下 spell 会到处标红波浪线，太吵。下面手动只开 wrap，不开 spell。
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
 
 -- Force-start treesitter highlighting on markdown buffers.
 -- nvim-treesitter main branch's get_installed("parsers") doesn't see the markdown
